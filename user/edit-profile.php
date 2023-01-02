456eb7a319ea784de3b02b6563d57e4e9a28b5a5
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2023-01-02 16:11:09 +0100

    has problem up edit or update pic

--- user/edit-profile.php
+++ user/edit-profile.php
@@ -1,37 +1,105 @@
 <?php include_once("common/header.php"); ?>
 
-<form action="" class="row mt-3" >
+<?php
+$sql = "SELECT * FROM users WHERE id = {$_SESSION['user_id']}";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+
+
+if (isset($_POST['update-btn'])) {
+    $name = $_POST['name'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $fb = $_POST['fb'];
+    $instagram = $_POST['instagram'];
+    $twitter = $_POST['twitter'];
+
+    if ($_FILES['new-picture']['name'] == "") {
+        $picture = $_POST['old-picture'];
+    } else {
+        $new_pic_name = $_FILES['new-picture']['name'];
+        $ext = strtolower(pathinfo($new_pic_name, PATHINFO_EXTENSION));
+        $size = $_FILES['new-picture']['size'] / 1024;
+        $valid_ext = ['png', 'jpg', 'jpeg'];
+
+        if (in_array($ext, $valid_ext)) {
+            if ($size > 2048) {
+                $msg = "<div class='alert alert-danger mt-3'>Picture size must not be greater than 2MB</div>";
+            } else {
+                $picture = time() . "." . $ext;
+                move_uploaded_file($_FILES['new-picture'][$new_name], "../assets/images/$picture");
+            }
+        } else {
+            $msg = "<div class='alert alert-danger mt-3'>Invalid file type , please choose (png, jpg, jpeg)</div>";
+        }
+
+        // UPDATE QUERY
+        $update = "UPDATE users SET user_name = '{$name}', gender = '{$gender}' , email = '{$email}', fb = '{$fb}', instagram = '{$instagram}', twitter = '{$twitter}' , picture = '{$picture}' WHERE id = {$_SESSION['user_id']}";
+
+        if (mysqli_query($conn, $update)) {
+            header("Location:{$URL}/user/profile.php");
+        }
+    }
+}
+?>
+
+<h3 class="mt-4">Update Profile</h3>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data" class="row mt-3">
     <div class="form-group col-md-6">
-        <input type="text" class="form-control" name="name" id="name" placeholder="Enter your name">
+        <input type="text" class="form-control" name="name" id="name" placeholder="Enter your name" value="<?php echo $row['user_name']; ?>">
         <small></small>
     </div>
 
     <div class="form-group col-md-6">
         <select class="form-control" name="gender" id="gender">
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-            <option value="Custom">Custom</option>
+            <option value="">Select Gender</option>
+
+            <?php
+            if ($row['gender']  == "Male") {
+                echo "
+                <option selected value='Male'>Male</option>
+                <option value='Female'>Female</option>
+                <option value='Custom'>Custom</option>
+                ";
+            } else if ($row['gender'] == "Female") {
+                echo "
+                <option value='Male'>Male</option>
+                <option selected  value='Female'>Female</option>
+                <option value='Custom'>Custom</option>
+                ";
+            } else if ($row['gender'] == "Custom") {
+                echo "
+                <option value='Male'>Male</option>
+                <option value='Female'>Female</option>
+                <option  selected  value='Custom'>Custom</option>
+                ";
+            }
+            ?>
+
+
         </select>
         <small></small>
     </div>
     <div class="form-group col-md-6">
-        <input type="email" class="form-control" name="email" id="email" placeholder="Enter your email">
+        <input type="email" class="form-control" name="email" id="email" placeholder="Enter your email" value="<?php echo $row['email']; ?>">
         <small></small>
     </div>
     <div class="form-group col-md-6">
-        <input type="text" class="form-control" name="facebook" id="facebook" placeholder="Enter your Facebook">
+        <input type="text" class="form-control" name="fb" id="facebook" placeholder="Enter your Facebook" value="<?php echo $row['fb']; ?>">
         <small></small>
     </div>
     <div class="form-group col-md-6">
-        <input type="text" class="form-control" name="instagram" id="instagram" placeholder="Enter your Instagram">
+        <input type="text" class="form-control" name="instagram" id="instagram" placeholder="Enter your Instagram" value="<?php echo $row['instagram']; ?>">
         <small></small>
     </div>
     <div class="form-group col-md-6">
-        <input type="text" class="form-control" name="twitter" id="twitter" placeholder="Enter your Twitter">
+        <input type="text" class="form-control" name="twitter" id="twitter" placeholder="Enter your Twitter" value="<?php echo $row['twitter']; ?>">
         <small></small>
     </div>
     <div class="form-group col-md-12">
-        <input type="file" class="form-control" name="picture" id="picture">
+        <input type="hidden" name="old-picture" value="<?php echo $row['picture']; ?>">
+        <input type="file" class="form-control" name="new-picture" id="picture">
+        <img src="../assets/images/<?php echo $row['picture']; ?>" class="img-thumbnail mt-3" width="140px" alt="">
         <small></small>
     </div>
 
@@ -40,5 +108,7 @@
         <small></small>
     </div>
 </form>
+<?php if (isset($msg)) echo $msg ?>
+
 
 <?php include_once("common/footer.php"); ?>
